7e8b5ad06f9e6461ed78c1c545b3fcf6281d31da
phocavalcante <pho.cavalcante@gmail.com>
2022-07-09 14:39:31 -0500

    Update 03.Programacao.e.Erros.py

--- A. O que é preciso para começar/03.Programacao.e.Erros.py
+++ A. O que é preciso para começar/03.Programacao.e.Erros.py
@@ -12,24 +12,24 @@
     
     A. COPIE A ÚLTIMA LINHA DO CÓDIGO E PESQUISE NO GOOGLE
         ISSO RESOLVE GRANDE PARTE DOS PROBLEMAS
+'''
+Print('Olá mundo!')
+
+''' 
+    RETORNA O ERRO ABAIXO:
+
+    line 23, in <module>
+        Print('Olá mundo!')
+    NameError: name 'Print' is not defined # copie essa linha e cole no google
+
+    RETORNO DA PESQUISA GOOGLE
+
+    "Os nomes de função e palavra-chave diferenciam maiúsculas de minúsculas em Python. Parece que você digitou Print onde quis dizer print"
 
 3. VOCÊ QUER FAZER ALGO, MAS NÃO SABE COMO?
         PASSO 1
             PENSE EM PALAVRAS CHAVES
         PASSO 2
             ESTRUTURE SUA PESQUISA > PALAVRAS CHAVES + LINGUAGEM DE PROGRAMAÇÃO
-            PREFERÊNCIA AO INDIOMA INGLÊS
-'''
-Print('Olá mundo!')
-
-''' 
-RETORNA O ERRO ABAIXO:
-
-line 23, in <module>
-    Print('Olá mundo!')
-NameError: name 'Print' is not defined # copie essa linha e cole no google
-
-RETORNO DA PESQUISA GOOGLE
-
-Os nomes de função e palavra-chave diferenciam maiúsculas de minúsculas em Python. Parece que você digitou Print onde quis dizer print
+            PREFERÊNCIA AO IDIOMA INGLÊS
 '''
